16a4e7934cd486b04464479ca331effeb3923b80
Your Name <your-email@example.com>
2026-05-09 23:30:22 -0400

    Fix CORS to allow all origins

--- api/create-subscription.js
+++ api/create-subscription.js
@@ -31,7 +31,7 @@ async function getOrCreatePrice(tier) {
 
 module.exports = async function handler(req, res) {
   // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://sparkdate.date');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
